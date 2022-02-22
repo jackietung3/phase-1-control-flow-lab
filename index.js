5b75d44046ca166e5bd2ae83a4e7575c2a011d5a
Jackie Tung <jackietung3@gmail.com>
2022-02-22 17:54:42 -0500

    done

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(i){
+  if (i <= 400){
+    return 'This one is on me!'
+  }
+  else if (i > 2500){
+    return "No can do.";
+  }
+  else {
+    return "I will gladly take your thirty bucks."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+        return "Thank you so much.";
+    case 'not as generous':
+        return "Thank you.";
+    case 'thanks for everything':
+        return "Bye.";
+  } 
 }
